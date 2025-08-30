d63c8c700cabef9fd31cc8fb5b64261247c313a9
sy469295-code <sy469295@gmail.com>
2025-08-30 22:16:24 +0545

    Add files via upload

---
+++ 1st 10 natural nos.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    i=1;
+    printf("\nThe first 10 natural number are: ");
+    while(i<=10)
+    {
+        printf("%d ",i);
+        i++;
+    }
+}
